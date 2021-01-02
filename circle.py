3632442aa1c1ec159b03e2235fc2b5d894866f4b
whitehat98xx <64109629+whitehat98xx@users.noreply.github.com>
2021-01-02 21:37:15 +0100

    circle

---
+++ circle.py
@@ -0,0 +1,78 @@
+import turtle
+
+#wn = turtle.Screen()
+#wn.bgcolor("black")
+#my_turle = turtle.Turtle()
+#my_turle.pencolor("blue")
+#my_turle.forward(100)
+#my_turle.left(90)
+#my_turle.pencolor("red")
+#my_turle.forward(100)
+#my_turle.left(90)
+#my_turle.pencolor("green")
+#my_turle.forward(100)
+#my_turle.pencolor("yellow")
+#my_turle.left(90)
+#my_turle.forward(100)
+#########################################################
+my_turle = turtle.Turtle()
+def square():
+  wn = turtle.Screen()
+  wn.bgcolor("black")
+  
+  my_turle.pencolor("blue")
+  my_turle.forward(100)
+  my_turle.left(90)
+  my_turle.pencolor("red")
+  my_turle.forward(100)
+  my_turle.left(90)
+  my_turle.pencolor("green")
+  my_turle.forward(100)
+  my_turle.pencolor("yellow")
+  my_turle.left(90)
+  my_turle.forward(100)
+  
+
+square()
+for i in range(20):
+  square()
+  my_turle.right(10)
+import turtle
+
+#wn = turtle.Screen()
+#wn.bgcolor("black")
+#my_turle = turtle.Turtle()
+#my_turle.pencolor("blue")
+#my_turle.forward(100)
+#my_turle.left(90)
+#my_turle.pencolor("red")
+#my_turle.forward(100)
+#my_turle.left(90)
+#my_turle.pencolor("green")
+#my_turle.forward(100)
+#my_turle.pencolor("yellow")
+#my_turle.left(90)
+#my_turle.forward(100)
+#########################################################
+my_turle = turtle.Turtle()
+def square():
+  wn = turtle.Screen()
+  wn.bgcolor("black")
+  
+  my_turle.pencolor("blue")
+  my_turle.forward(100)
+  my_turle.left(90)
+  my_turle.pencolor("red")
+  my_turle.forward(100)
+  my_turle.left(90)
+  my_turle.pencolor("green")
+  my_turle.forward(100)
+  my_turle.pencolor("yellow")
+  my_turle.left(90)
+  my_turle.forward(100)
+  
+
+square()
+for i in range(20):
+  square()
+  my_turle.right(10)
